6b3d4d8895fa254aa5a477be8921ea521d5bf61f
Josuepma <1930168@upv.edu.mx>
2022-03-31 07:54:18 -0600

    Agregando functiones al controlador

--- app/Http/Controllers/PublicacionesController.php
+++ app/Http/Controllers/PublicacionesController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Publicacion;
 
 class PublicacionesController extends Controller
 {
-    //
+    public function find($id){
+        return Publicacion::findOrFail();
+    }
+
+    public function getAll(){
+        return Publicacion::all();
+    }
+
+    public function create(Request $req){
+        return Publicacion::create($req->all());
+    }
 }
